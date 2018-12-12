0bbde527baa776a02073a5380f647f767266ea6f
ryanboris <ryan.boris@gmail.com>
2018-12-12 14:47:27 -0500

    fixes issues

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,7 @@
 // Update with your config settings.
 
 require('dotenv').config()
-const localPgConnection = {
-	host     : process.env.HOSTSER, // address to find the db server
-	database : process.env.DBNAME,
-	user     : process.env.USERDB,
-	password : process.env.DBPASS,
-}
+
 // where is DATABASE_URL coming from?
 const dbConnection = process.env.DATABASE_URL || localPgConnection
 
