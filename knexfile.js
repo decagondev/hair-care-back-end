6089d5dba1c263b4986b1043018dbe60d9570f0b
ryanboris <ryan.boris@gmail.com>
2018-12-12 14:44:36 -0500

    fixes issues

--- knexfile.js
+++ knexfile.js
@@ -1,43 +1,42 @@
 // Update with your config settings.
+
 require('dotenv').config()
 const localPgConnection = {
-	host: process.env.PORT, // address to find the db server
-	database: 'hair-care-back-end',
-	user: 'ryan',
-	password: 'pass',
-  };
-  // where is DATABASE_URL coming from?
-  const dbConnection = process.env.DATABASE_URL || localPgConnection;
-  
-  module.exports = {
-	development: {
-	  client: 'sqlite3',
-	  connection: {
-		filename: './db/lambda.sqlite3',
-	  },
-	  migrations: {
-		tableName: 'knex_migrations',
-		directory: './db/migrations',
-	  },
-	  seeds: {
-		directory: './db/seeds',
-	  },
-	  useNullAsDefault: true, // not needed for production only for sqlite
+	host     : process.env.HOSTSER, // address to find the db server
+	database : process.env.DBNAME,
+	user     : process.env.USERDB,
+	password : process.env.DBPASS,
+}
+// where is DATABASE_URL coming from?
+const dbConnection = process.env.DATABASE_URL || localPgConnection
+
+module.exports = {
+	development : {
+		client           : 'sqlite3',
+		connection       : {
+			filename : './data/database.sqlite3',
+		},
+		useNullAsDefault : true,
+		migrations       : {
+			directory : './data/migrations/',
+		},
+		seeds            : {
+			directory : './data/seeds',
+		},
+		production       : {
+			client     : 'pg',
+			connection : dbConnection, // can be and object or a string
+			pool       : {
+				min : 2,
+				max : 10,
+			},
+			migrations : {
+				tableName : 'knex_migrations',
+				directory : './db/migrations',
+			},
+			seeds      : {
+				directory : './db/seeds',
+			},
+		},
 	},
-	production: {
-	  client: 'pg',
-	  connection: dbConnection, // can be and object or a string
-	  pool: {
-		min: 2,
-		max: 10,
-	  },
-	  migrations: {
-		tableName: 'knex_migrations',
-		directory: './db/migrations',
-	  },
-	  seeds: {
-		directory: './db/seeds',
-	  },
-	},
-  };
-  
+}
