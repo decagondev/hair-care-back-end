2f6f981c95cb71e07fe1cb3a3ba6f09808a10025
ryanboris <ryan.boris@gmail.com>
2018-12-12 14:28:59 -0500

    knexfile update

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,6 @@
 // Update with your config settings.
-
+require('dotenv').config()
+const dbConnection = process.env.DATABASE_URL
 module.exports = {
 	development : {
 		client           : 'sqlite3',
@@ -12,6 +13,21 @@ module.exports = {
 		},
 		seeds            : {
 			directory : './data/seeds',
+		},
+		production       : {
+			client     : 'pg',
+			connection : dbConnection, // can be and object or a string
+			pool       : {
+				min : 2,
+				max : 10,
+			},
+			migrations : {
+				tableName : 'knex_migrations',
+				directory : './db/migrations',
+			},
+			seeds      : {
+				directory : './db/seeds',
+			},
 		},
 	},
 }
